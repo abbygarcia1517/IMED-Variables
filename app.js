4353fbe416da2212ba0c694bfaa55e20895b364b
Jonathan Garcia <jongar737@gmail.com>
2025-02-26 16:03:08 -0600

    Final

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
+var name = "Phone Battery" //string variable 
+var battery = 100;
+battery = battery - 20; //mathematical operations on variables 
 
+console.log(name);
 console.log(battery);
 
-var battery = 100; //This is my the full battery life of my phone 
-
-battery = battery - 20;
-battery = battery - 10;
-
-//90
-
-var name = "Phone Battery"; //This is the display name of my phone battery life 
-
-var signal = true; //BOOLEAN
-
-let a;
-let b;
-
-a = 20;
-b = 80;
-
-let c = a + b; //block variable
 
 
 
- // global variable 
-    //string variable 
-    // object variable using const
+//const object 
+const phone = {type:"iphone", model:"16pro", color:"silver"};
+
