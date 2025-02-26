3a70c00e2f443601ac4792b82b9cf6dbb950f199
Jonathan Garcia <jongar737@gmail.com>
2025-02-26 15:08:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IMED-Variables
+ 
---
+++ app.js
@@ -0,0 +1,27 @@
+
+console.log(battery);
+
+var battery = 100; //This is my the full battery life of my phone 
+
+battery = battery - 20;
+battery = battery - 10;
+
+//90
+
+var name = "Phone Battery"; //This is the display name of my phone battery life 
+
+var signal = true; //BOOLEAN
+
+let a;
+let b;
+
+a = 20;
+b = 80;
+
+let c = a + b; //block variable
+
+
+
+ // global variable 
+    //string variable 
+    // object variable using const
